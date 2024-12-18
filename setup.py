dea6474a985950aebf52aae6d68177a8bcec8f1b
BernieTelalovic <bernie.books@live.com>
2024-12-18 15:11:42 +0100

    Fixing python version dependency.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     test_suite='tests',
     entry_points={
         'console_scripts': [
